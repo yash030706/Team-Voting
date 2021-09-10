9be5799de91b1bda9676feb3d0ad761241904ee1
yash030706 <yashaggarwal0307@gmail.com>
2021-09-10 18:36:54 +0530

    Adding the firebase

--- config.js
+++ config.js
@@ -1,10 +1,16 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+    apiKey: "AIzaSyA-FX_3RpqX7jzFU-6xjI0USq6Bq6hM-T8",
+    authDomain: "teamvoting-c1fd5.firebaseapp.com",
+    databaseURL: "https://teamvoting-c1fd5-default-rtdb.firebaseio.com",
+    projectId: "teamvoting-c1fd5",
+    storageBucket: "teamvoting-c1fd5.appspot.com",
+    messagingSenderId: "412034100557",
+    appId: "1:412034100557:web:9fae7b8a09c9759c8f1b04"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
 export default firebase.database();
